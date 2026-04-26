0311336c9a9b0b770533e1cbcf2fe31a134de37d
Paweł Walaszek <pawel.walaszek@eengine.pl>
2026-04-26 23:15:20 +0200

    Allow local Meet2Note callback origin

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -11,3 +11,5 @@ test -f dist/connect-callback.html
 test -f dist/connectCallback.js
 grep -q 'connect-callback.html' dist/manifest.json
 grep -F -q 'https://meet2note.com/*' dist/manifest.json
+grep -F -q 'http://localhost/*' dist/manifest.json
+grep -F -q 'http://127.0.0.1/*' dist/manifest.json
